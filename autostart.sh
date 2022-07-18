dec3537a1cb17824a920694420b2c696b0f6edf4
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-07-18 21:01:14 +0200

    Init 🥰

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+stickykeys & # stikcykeys script
+picom & # Start compositor , for transparency
+nitrogen --restore & # Restores the wallpaper
+nm-applet & # Start the network manger
+copyq & # clipboard manger
